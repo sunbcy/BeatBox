3b85c613126a2dfd4707027dceabf7a20a705c82
bcy <626813534@qq.com>
2025-02-24 23:09:35 +0800

    Initial commit of Chapter20-23

---
+++ app/src/main/java/com/bcy/android/beatbox/BeatBoxFragment.java
@@ -0,0 +1,9 @@
+package com.bcy.android.beatbox;
+
+import androidx.fragment.app.Fragment;
+
+public class BeatBoxFragment extends Fragment {
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+}
