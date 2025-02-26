6cfaf9dd4bda3b6239bb41fef4068e1ba5b73328
bcy <626813534@qq.com>
2025-02-27 02:15:03 +0800

    20-5 把一般布局改造为数据绑定布局;20-6 实例化绑定类

--- app/src/main/java/com/bcy/android/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/bcy/android/beatbox/BeatBoxFragment.java
@@ -1,9 +1,23 @@
 package com.bcy.android.beatbox;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+
+import com.bcy.android.beatbox.databinding.FragmentMainBinding;
 
 public class BeatBoxFragment extends Fragment {
     public static BeatBoxFragment newInstance() {
         return new BeatBoxFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        FragmentMainBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false);
+        return binding.getRoot();
     }
 }
